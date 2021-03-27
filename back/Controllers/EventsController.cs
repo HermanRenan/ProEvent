feff947fa008b69e852bfe106955294ec39f1b9b
Renan <renan.herman@hotmail.com>
2021-03-27 13:42:42 -0300

    Secção 2 >net 5

---
+++ back/Controllers/EventsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using projectFullS.Models;
+
+namespace projectFullS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventsController : ControllerBase
+    {    
+
+            public IEnumerable<Event> _event = new Event[]
+            {   
+                new Event(){
+                EventId = 1,
+                QtdPessoas= 2,
+                Local="BH",
+                DataEvent= DateTime.Now.AddDays(2).ToString(),
+                Lote= "1º Lote"
+                },
+                new Event(){
+                EventId = 2,
+                QtdPessoas= 212,
+                Local="SP",
+                DataEvent= DateTime.Now.AddDays(2).ToString(),
+                Lote= "2º Lote"
+                },
+            };
+
+        public EventsController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Event> Get()
+        {
+            return _event;     
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Event> Get(int id)
+        {
+            return _event.Where(x=> x.EventId == id);     
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "post";
+        }
+    }
+}
---
+++ back/Models/Events.cs
@@ -0,0 +1,13 @@
+namespace projectFullS.Models
+{
+    public class Event
+    {
+        public int EventId {get; set;}
+        public string Local {get; set;}
+        public string DataEvent  {get; set;}
+        public int QtdPessoas {get; set;}
+        public string Lote {get; set;}    
+        public string ImageURL {get; set;}
+        
+    }
+}
